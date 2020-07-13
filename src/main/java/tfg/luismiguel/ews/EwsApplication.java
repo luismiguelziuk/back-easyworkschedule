6c301ca85e4d496601635a27f12b047c7effd015
lmgarciar <luismiguel.garcia@iteriam.es>
2020-07-13 17:53:35 +0200

    Configuracion inicial spring-boot y mysql

---
+++ src/main/java/tfg/luismiguel/ews/EwsApplication.java
@@ -0,0 +1,13 @@
+package tfg.luismiguel.ews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tfg/luismiguel/ews/EwsApplicationTests.java
@@ -0,0 +1,13 @@
+package tfg.luismiguel.ews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
