6f6e7e919bbcdf1e896680bf2497fbde15c852a1
lmgarciar <luismiguel.garcia@iteriam.es>
2020-09-02 17:56:16 +0200

    Version 1.0.3 Fix endpoint excel

--- src/main/java/tfg/luismiguel/ews/controller/ExcelController.java
+++ src/main/java/tfg/luismiguel/ews/controller/ExcelController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +33,7 @@ public class ExcelController {
                     content = @Content),
             @ApiResponse(responseCode = "404", description = "Excel not generated",
                     content = @Content)})
-    @GetMapping("/generate/excel")
+    @PutMapping("/generate/excel")
     public ResponseEntity<byte[]> generateExcel(@RequestBody GenerateExcelDTO generateExcelDTO) throws EwsException, IOException {
         byte[] excelFileInBytes = excelService.generateExcel(generateExcelDTO);
         if (excelFileInBytes == null) {
